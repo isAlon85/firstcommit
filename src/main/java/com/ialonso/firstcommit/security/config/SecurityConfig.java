5a1eec94e88ce8b1db575c96c217fa796d17a82c
Israel Alonso <93823993+isAlon85@users.noreply.github.com>
2022-01-21 20:57:03 +0100

    added vercel app in cors v2

--- src/main/java/com/ialonso/firstcommit/security/config/SecurityConfig.java
+++ src/main/java/com/ialonso/firstcommit/security/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.setAllowedOrigins(List.of("http://localhost"/*, "https://angular-springboot-*.vercel.app"*/));
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://https://ob-firstcommit-react.vercel.app/"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://ob-firstcommit-react.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
         configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true);
